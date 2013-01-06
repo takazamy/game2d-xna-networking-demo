6bb32e21b49bc593fa3050d8cf239ba86c3a93b3
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@9cb20eb2-1db2-f0ea-0614-e5998b7f2738>
2013-01-06 02:53:48 +0000

    Update 1

--- trunk/WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
+++ trunk/WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
@@ -8,6 +8,8 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using WindowsGame1.Managers;
+using WindowsGame1.Core;
 
 namespace WindowsGame1
 {
@@ -18,10 +20,13 @@ namespace WindowsGame1
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        GameManager gameManager;
+       // TileMap tile;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            this.graphics.PreferredBackBufferWidth = 800;
+            this.graphics.PreferredBackBufferHeight = 600;
             Content.RootDirectory = "Content";
         }
 
@@ -33,8 +38,13 @@ namespace WindowsGame1
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            // TODO: Add your initialization logic here  
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            gameManager = new GameManager(this, spriteBatch);
+            //Texture2D tileset = this.Content.Load<Texture2D>("map");
+           // tile = new TileMap(this,"E:\\Hoc Tap\\XNA\\GameDemo2DNetworking\\Game\\game2d-xna-networking-demo\\WindowsGame1\\WindowsGame1\\WindowsGame1\\map1.json",tileset, this.spriteBatch);
+            this.Components.Add(gameManager);
+           // this.Components.Add(tile);
             base.Initialize();
         }
 
@@ -45,7 +55,7 @@ namespace WindowsGame1
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+           
 
             // TODO: use this.Content to load your game content here
         }
@@ -66,11 +76,21 @@ namespace WindowsGame1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
+           
 
             // TODO: Add your update logic here
+            KeyboardState state = Keyboard.GetState();
+
+             // Allows the game to exit
+            if ((GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)||state.IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            // Allows the game to FullScreen
+            if ((state.IsKeyDown(Keys.LeftControl)||state.IsKeyDown(Keys.RightControl))&&state.IsKeyDown(Keys.Enter))
+            {
+                this.graphics.ToggleFullScreen();
+            }
+
 
             base.Update(gameTime);
         }
@@ -84,7 +104,7 @@ namespace WindowsGame1
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+         
             base.Draw(gameTime);
         }
     }
--- trunk/WindowsGame1/WindowsGame1/WindowsGame1/Screens/GameScreen.cs
+++ trunk/WindowsGame1/WindowsGame1/WindowsGame1/Screens/GameScreen.cs
@@ -8,47 +8,23 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using WindowsGame1.Managers;
 
 
-namespace WindowsGame1.GameObjects
+namespace WindowsGame1.Screens
 {
     /// <summary>
     /// This is a game component that implements IUpdateable.
     /// </summary>
-    public class Character : Microsoft.Xna.Framework.DrawableGameComponent
+    public class GameScreen : Screen
     {
-
-        #region Properties
-        SpriteBatch spriteBatch;
-        public int hp;
-        public int damage;
-        public Vector2 location;
-        public Vector2 moveDirection;
-        public Vector2 shootDirection;
-        public Vector2 lookDirection;
-        protected Texture2D body;
-        protected Texture2D head;
-        public Boolean isMe;
-        #endregion
-
-        #region Contructors
-        public Character(Game game, SpriteBatch spriteBatch)
-            : base(game)
+        public GameScreen(ScreenManager scrManager, Game game, SpriteBatch spriteBatch)
+            : base(scrManager, game, spriteBatch)
         {
-            this.spriteBatch = spriteBatch;
+            this.state = Global.ScreenState.GS_MAIN_GAME;
             // TODO: Construct any child components here
         }
 
-        public Character(Game game, SpriteBatch spriteBatch, Vector2 Location,int HP, int Damage)
-            : base(game)
-        {
-            this.spriteBatch = spriteBatch;
-            this.location = Location;
-            this.hp = HP;
-            this.damage = Damage;
-            // TODO: Construct any child components here
-        }
-        #endregion
         /// <summary>
         /// Allows the game component to perform any initialization it needs to before starting
         /// to run.  This is where it can query for any required services and load content.
@@ -60,11 +36,6 @@ namespace WindowsGame1.GameObjects
             base.Initialize();
         }
 
-        protected override void LoadContent()
-        {
-
-            base.LoadContent();
-        }
         /// <summary>
         /// Allows the game component to update itself.
         /// </summary>
@@ -74,11 +45,6 @@ namespace WindowsGame1.GameObjects
             // TODO: Add your update code here
 
             base.Update(gameTime);
-        }
-
-        public override void Draw(GameTime gameTime)
-        {
-            base.Draw(gameTime);
         }
     }
 }
