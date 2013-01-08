4e976cbb02c34d9c49d7e12739d3fc3c60b5b4b2
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@9cb20eb2-1db2-f0ea-0614-e5998b7f2738>
2013-01-08 19:02:34 +0000

    Update Full 9/1/2013

--- trunk/WindowsGame1/WindowsGame1/WindowsGame1/Core/MouseComponent.cs
+++ trunk/WindowsGame1/WindowsGame1/WindowsGame1/Core/MouseComponent.cs
@@ -18,6 +18,8 @@ namespace WindowsGame1.Core
     public class MouseComponent : Microsoft.Xna.Framework.DrawableGameComponent
     {
         SpriteBatch spriteBatch;
+        Texture2D mouseImage;
+        Vector2 location = Vector2.Zero;
         public MouseComponent(Game game, SpriteBatch spriteBatch)
             : base(game)
         {
@@ -38,6 +40,7 @@ namespace WindowsGame1.Core
 
         protected override void LoadContent()
         {
+            mouseImage = this.Game.Content.Load<Texture2D>("crosshair");
             base.LoadContent();
         }
         /// <summary>
@@ -47,12 +50,17 @@ namespace WindowsGame1.Core
         public override void Update(GameTime gameTime)
         {
             // TODO: Add your update code here
-
+            MouseState state = Mouse.GetState();
+            this.location.X = state.X - this.mouseImage.Width / 2;
+            this.location.Y = state.Y - this.mouseImage.Height / 2;
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
+            spriteBatch.Begin();
+            spriteBatch.Draw(this.mouseImage,this.location, Color.White);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
--- trunk/WindowsGame1/WindowsGame1/WindowsGame1/Global.cs
+++ trunk/WindowsGame1/WindowsGame1/WindowsGame1/Global.cs
@@ -40,9 +40,45 @@ namespace WindowsGame1
             BS_HOLD = 1,
         }
         #endregion
+        ///<summary>
+        /// Find the angle between two vectors. This will not only give the angle difference, but the direction.
+        /// For example, it may give you -1 radian, or 1 radian, depending on the direction. Angle given will be the 
+        /// angle from the FromVector to the DestVector, in radians.
+        /// </summary>
+        /// <param name="FromVector">Vector to start at.</param>
+        /// <param name="DestVector">Destination vector.</param>
+        /// <param name="DestVectorsRight">Right vector of the destination vector</param>
+        /// <returns>Signed angle, in radians</returns>        
+        /// <remarks>All three vectors must lie along the same plane.</remarks>
+        ///       
+        public static double GetSignedAngleBetween2DVectors(Vector2 FromVector, Vector2 DestVector, Vector2 DestVectorsRight)
+        {
+            FromVector.Normalize();
+            DestVector.Normalize();
+            DestVectorsRight.Normalize();
 
+            float forwardDot = Vector2.Dot(FromVector, DestVector);
+            float rightDot = Vector2.Dot(FromVector, DestVectorsRight);
 
+            // Keep dot in range to prevent rounding errors
+            forwardDot = MathHelper.Clamp(forwardDot, -1.0f, 1.0f);
 
+            double angleBetween = Math.Acos(forwardDot);
+
+            if (rightDot < 0.0f)
+                angleBetween *= -1.0f;
+
+            return angleBetween;
+        }
+
+       
+        public static float UnsignedAngleBetweenTwoV2(Vector2 v1, Vector2 v2)
+        {
+            v1.Normalize();
+            v2.Normalize();
+            double Angle = (float)Math.Acos(Vector2.Dot(v1, v2));
+            return (float)Angle;
+        }
 
         public Global(Game game)
             : base(game)
